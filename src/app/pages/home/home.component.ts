0189971b5ce8f3e0a6c1b922203b29d224b16a00
Kevin Ruiz <desarrollador2@torodigital.com.co>
2024-02-09 10:54:14 -0500

    Migración de nueva sintaxis de Angular 17

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, computed, signal, effect } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { Task } from '../../models/tasks.model';
@@ -7,7 +7,7 @@ import { Task } from '../../models/tasks.model';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css',
 })
